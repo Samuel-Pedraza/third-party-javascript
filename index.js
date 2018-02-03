2c550f5414124b3d071b017a249a88a883f16758
sam <samdpedraza@gmail.com>
2018-02-03 12:15:08 -0800

    allow embed on cross domain for async

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 var myHtmlForm = "<form action='https://formspree.io/samdpedraza@gmail.com' method='POST'><input type='text' name='name'><input type='email' name='_replyto'><input type='submit' value='Send'> </form>";
 
-document.write(myHtmlForm);
+document.getElementsByTagName("body")[0].innerHTML = myHtmlForm;
