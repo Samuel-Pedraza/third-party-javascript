a3e209c55973a1cb3c9a1b97d930887393435351
sam <samdpedraza@gmail.com>
2018-02-03 14:04:34 -0800

    grab url of webpage

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-var myHtmlForm = "<form action='https://formspree.io/samdpedraza@gmail.com' method='POST'><input type='text' name='name'><input type='email' name='_replyto'><input type='submit' value='Send'> </form>";
+var myHtmlForm = "<form action='https://formspree.io/samdpedraza@gmail.com' method='POST'><input type='text' name='name'><input type='email' name='_replyto'><input type='hidden' name='myip'><input type='hidden' name='url' value='" + window.location.href + "'><input type='submit' value='Send'> </form>";
 
 document.getElementsByTagName("body")[0].innerHTML = myHtmlForm;
