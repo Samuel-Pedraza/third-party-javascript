048558fb988be5a7abc826b2b369bcdf0d3b244f
sam <samdpedraza@gmail.com>
2018-02-03 15:42:55 -0800

    almost got it going on

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-var myHtmlForm = "<form action='https://formspree.io/samdpedraza@gmail.com' method='POST'><input type='text' name='name'><input type='email' name='_replyto'><input type='hidden' name='myip'><input type='hidden' name='url' value='" + window.location.href + "'><input type='submit' value='Send'> </form>";
+var myscript = "function getIP(json) { document.getElementsByTagName('head')[0].innerHTML = 'My public IP address is: ' + json.ip; }"
+
+var myHtmlForm = "<script>" + myscript + "</\script><form action='https://formspree.io/samdpedraza@gmail.com' method='POST'><input type='email' name='_replyto' placeholder='email'><br><textarea name='specify' placeholder='Specify a particulary product on this page'></textarea><input type='hidden' name='myip' onload='getIP(json).value'><input type='hidden' name='url' value='" + window.location.href + "'><br><input type='submit' value='Send'></form><script type='application/javascript' src='https://api.ipify.org?format=jsonp&callback=getIP'></\script>";
 
 document.getElementsByTagName("body")[0].innerHTML = myHtmlForm;
