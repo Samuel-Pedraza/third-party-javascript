9fb3ed3bd9dde356cc57e9b0ec25bcfb7980c2bd
sam <samdpedraza@gmail.com>
2018-02-03 23:32:01 -0800

    Added ability to specify quantity

--- index.js
+++ index.js
@@ -41,6 +41,6 @@ returnMyLocation = function() {
 };
 
 
-var myHtmlForm = "<form action='https://formspree.io/samdpedraza@gmail.com' method='POST'><input type='email' name='_replyto' placeholder='email'><br><textarea name='specify' placeholder='Specify a particulary product on this page'></textarea><input type='hidden' id='myip' name='myip' value=''><input type='hidden' name='url' value='" + window.location.href + "'><br><input type='submit' value='Send'></form><script type='application/javascript' src='https://api.ipify.org?format=jsonp&callback=getIP'></\script>";
+var myHtmlForm = "<form action='https://formspree.io/samdpedraza@gmail.com' method='POST'><input type='email' name='_replyto' placeholder='email'><br><input type='text' name='qty' placeholder='specify quantity'><textarea name='specify' placeholder='Specify a particulary product on this page'></textarea><input type='hidden' id='myip' name='myip' value=''><input type='hidden' name='url' value='" + window.location.href + "'><br><input type='submit' value='Send'></form><script type='application/javascript' src='https://api.ipify.org?format=jsonp&callback=getIP'></\script>";
 
 document.getElementsByTagName("body")[0].innerHTML = myHtmlForm;
